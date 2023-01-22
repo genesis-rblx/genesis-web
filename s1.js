a24cd2e936ba142fd5046ef685521e610d5b6d5f
abhipin <abhinavpin@gmail.com>
2023-01-22 16:48:51 +0530

    implement validation for booking form

--- s1.js
+++ s1.js
@@ -1,3 +1,6 @@
+var agreementPrimary = false;
+var agreementSecondary = false;
+
 function updateFormDropdown(text, id) {
     const button = document.getElementById(id)
     button.innerHTML = '&nbsp' + text
@@ -6,21 +9,34 @@ function updateFormDropdown(text, id) {
 function postFormData() {
 
     const mandatoryInput = [document.getElementById('Username'), document.getElementById('DiscordUsernameNTag'), document.getElementById('TargetLaunchDate'), document.getElementById('PayloadReadyDate'), document.getElementById('Inclination'), document.getElementById('Altitude')]
-
     
     var xhr = new XMLHttpRequest();
-  
-    
     xhr.open("POST", "https://genesiswebsiteservice.shaperorblx.repl.co/", true);
 
     var formData = {};
-        
+    var validationPassed = true;
+
     mandatoryInput.forEach(function(item, index){
-        formData[item.id] = item.value
+        if (!item.validity.valueMissing) {
+            formData[item.id] = item.value
+        } else {
+            console.warn("Invalid input")
+            validationPassed = false;
+        }
     })
+
+    if (!agreementPrimary || !agreementSecondary) {
+        validationPassed = false;
+    }
+
+    if (!validationPassed) {
+        document.getElementById("errorMessagePresenter").innerHTML = "INCOMPLETE FORM"
+        return;
+    }
 
     
     xhr.send(JSON.stringify(formData));
+    
     window.location.href = "index.html"
 }
 
@@ -33,4 +49,10 @@ function updateAgreementButtonAnim(divid, textid, buttonid) {
     button.style.backgroundColor = "rgb(255, 111, 111)";
     text.style.color = "rgb(255, 79, 79)";
     div.style.borderColor= "rgb(255, 111, 111)";
+
+    if (divid == "da-1") {
+        agreementPrimary = true;
+    } else if (divid == "da-2") {
+        agreementSecondary = true
+    }
 }
