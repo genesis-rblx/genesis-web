51b11f242945da8a30552b7ab86b98cc10385356
abhipin <abhinavpin@gmail.com>
2023-01-27 10:04:16 +0530

    fix bug where text would not center on first load

--- responsive.js
+++ responsive.js
@@ -1,10 +1,12 @@
-const CoverImage = document.getElementById('cover-image')
-const Navbar = document.getElementById('navbar')
+window.addEventListener('load', function(){
+    const CoverImage = document.getElementById('cover-image')
+    const Navbar = document.getElementById('navbar')
 
-var imgBoundsCrossed = (CoverImage.offsetHeight + Navbar.offsetHeight) > document.body.clientHeight
+    var imgBoundsCrossed = (CoverImage.offsetHeight + Navbar.offsetHeight) > document.body.clientHeight
 
-if (imgBoundsCrossed) {
-    const Text = document.getElementById("dsized-responsive")
+    if (imgBoundsCrossed) {
+        const Text = document.getElementById("dsized-responsive")
 
-    Text.style.top = String(parseInt(document.body.clientHeight/2 - Navbar.offsetHeight)) + "px";
-}
+        Text.style.top = String(parseInt(document.body.clientHeight/2 - Navbar.offsetHeight)) + "px";
+    }
+})
