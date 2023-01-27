3dc6f071f41d1704808e82e7447a07923e4d6f1c
abhipin <abhinavpin@gmail.com>
2023-01-27 09:59:28 +0530

    update image text to center on large resolutions

---
+++ responsive.js
@@ -0,0 +1,10 @@
+const CoverImage = document.getElementById('cover-image')
+const Navbar = document.getElementById('navbar')
+
+var imgBoundsCrossed = (CoverImage.offsetHeight + Navbar.offsetHeight) > document.body.clientHeight
+
+if (imgBoundsCrossed) {
+    const Text = document.getElementById("dsized-responsive")
+
+    Text.style.top = String(parseInt(document.body.clientHeight/2 - Navbar.offsetHeight)) + "px";
+}
