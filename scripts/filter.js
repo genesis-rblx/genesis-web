463b5db07f84b7426bf0d18a5c6a133b6490638e
abhipin <abhinavpin@gmail.com>
2023-05-31 12:12:14 +0530

    implement search feature

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,11 +1,12 @@
 const DepartmentFilter = document.getElementById("FilterDepartment")
 const LocationFilter = document.getElementById("FilterLocation")
-
+const JobFilter = document.getElementById("FilterJobName")
 const ApplicationList = document.getElementById("applications")
 
-const Observer = new MutationObserver(() => {
+function callback() {
     const Department = (DepartmentFilter.innerText.trim() != "Department") ? DepartmentFilter.innerText.trim() : false
     const Location = (LocationFilter.innerText.trim() != "Location") ? LocationFilter.innerText.trim() : false
+    const Job = JobFilter.value.length > 0 ? JobFilter.value.trim() : false
 
     for (let i = 0; i < ApplicationList.children.length; i++) {
 
@@ -19,10 +20,15 @@ const Observer = new MutationObserver(() => {
             }
         }
         
-
         if (Location) {
             if (Row.children[1].firstElementChild.innerText.trim() != Location) {
                 show = false;
+            }
+        }
+
+        if (Job) {
+            if (!(Row.children[0].firstElementChild.innerText.toLowerCase().match(Job))) {
+                show = false
             }
         }
 
@@ -33,49 +39,11 @@ const Observer = new MutationObserver(() => {
         } else {
             Row.classList.add("d-none")
         }
-        
-
-        /*
-        if (Department) {
-            if (ApplicationList.children[i].children[2].firstElementChild.innerText.trim() != Department) {
-                if (!ApplicationList.children[i].classList.contains("d-none")) {
-                    ApplicationList.children[i].classList.add("d-none")
-                }
-            } else {
-                if (ApplicationList.children[i].classList.contains("d-none")) {
-                    ApplicationList.children[i].classList.remove("d-none")
-                }
-            }
-        } else {
-            if (ApplicationList.children[i].classList.contains("d-none")) {
-                ApplicationList.children[i].classList.remove("d-none")
-            }
-        }
-
-        if (Location) {
-
-            if (Department) {
-                if (ApplicationList.children[i].children[2].firstElementChild.innerText.trim() != Department)
-            }
-
-            if (ApplicationList.children[i].children[1].firstElementChild.innerText.trim() != Location) {
-                if (!ApplicationList.children[i].classList.contains("d-none")) {
-                    ApplicationList.children[i].classList.add("d-none")
-                }
-            } else {
-                if (!ApplicationList.children[i].classList.contains("d-none")) {
-                    ApplicationList.children[i].classList.remove("d-none")
-                }
-            }
-        } else {
-            if (ApplicationList.children[i].classList.contains("d-none")) {
-                ApplicationList.children[i].classList.remove("d-none")
-            }
-        }
-        */
     }
+}
 
-});
+const Observer = new MutationObserver(callback);
 
 Observer.observe(DepartmentFilter, { subtree: true, childList: true })
 Observer.observe(LocationFilter, { subtree: true, childList: true })
+JobFilter.addEventListener('input', callback);
